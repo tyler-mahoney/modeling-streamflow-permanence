924978366fb55bb50eb573bc5692a519a68e4a97
tyler-mahoney <tyler.mahoney@louisville.edu>
2022-07-06 14:58:19 -0400

    Updated the downstream cell ID algorithm

--- downstream-cells-quantification.R
+++ downstream-cells-quantification.R
@@ -10,7 +10,6 @@ fdr.matrix <- as.matrix(FR.fdr)
 fdr.downstream.matrix <- matrix(nrow=nrow(HRU.matrix),ncol=ncol(HRU.matrix))
 
 
-
 for (i in 1:nrow(HRU.matrix)) {
   for (j in 1:ncol(HRU.matrix)) {
     if (is.na(fdr.matrix[i,j])) {
@@ -19,19 +18,19 @@ for (i in 1:nrow(HRU.matrix)) {
       if (fdr.matrix[i,j]==1) {
         fdr.downstream.matrix[i,j] <- HRU.matrix[i,j+1]
       } else if (fdr.matrix[i,j]==2) {
-        fdr.downstream.matrix[i,j] <- HRU.matrix[i-1,j+1]
+        fdr.downstream.matrix[i,j] <- HRU.matrix[i+1,j+1]
       } else if (fdr.matrix[i,j]==4) {
-        fdr.downstream.matrix[i,j] <- HRU.matrix[i-1,j]
+        fdr.downstream.matrix[i,j] <- HRU.matrix[i+1,j]
       } else if (fdr.matrix[i,j]==8) {
-        fdr.downstream.matrix[i,j] <- HRU.matrix[i-1,j-1]
+        fdr.downstream.matrix[i,j] <- HRU.matrix[i+1,j-1]
       } else if (fdr.matrix[i,j]==16) {
         fdr.downstream.matrix[i,j] <- HRU.matrix[i,j-1]
       } else if (fdr.matrix[i,j]==32) {
-        fdr.downstream.matrix[i,j] <- HRU.matrix[i+1,j-1]
+        fdr.downstream.matrix[i,j] <- HRU.matrix[i-1,j-1]
       } else if (fdr.matrix[i,j]==64) {
-        fdr.downstream.matrix[i,j] <- HRU.matrix[i+1,j]
+        fdr.downstream.matrix[i,j] <- HRU.matrix[i-1,j]
       } else if (fdr.matrix[i,j]==128) {
-        fdr.downstream.matrix[i,j] <- HRU.matrix[i+1,j+1]
+        fdr.downstream.matrix[i,j] <- HRU.matrix[i-1,j+1]
       } else {
         fdr.downstream.matrix[i,j] <- NA
       }
@@ -39,61 +38,23 @@ for (i in 1:nrow(HRU.matrix)) {
   }
 }
 
+downstream.weighting.matrix <- matrix(nrow=length(unique(FR.HRU)),ncol=length(unique(FR.HRU)))
 
-for (i in 1:20) {
-  for (j in 1:20) {
-    fdr.downstream.matrix[i,j] <- i+j
+for (i in 1:length(unique(FR.HRU))) {
+  for (j in 1:length(unique(FR.HRU))) {
+    print(unique(FR.HRU)[i])
+    downstream.weighting.matrix[i,j] <- length(which((HRU.matrix==unique(FR.HRU)[i])&(fdr.downstream.matrix==unique(FR.HRU)[j])))
   }
 }
 
-fdr.matrix[1000,1000]==64
+options("scipen"=100, "digits"=5)
 
+rownames(downstream.weighting.matrix) <- unique(FR.HRU)
+colnames(downstream.weighting.matrix) <- unique(FR.HRU)
 
-i=1
-j=1
+weighting.matrix <- downstream.weighting.matrix/rowSums(downstream.weighting.matrix)
 
-if (is.na(fdr.matrix[i,j])) {
-  fdr.downstream.matrix[i,j] <- NA
-} else {
-  if (fdr.matrix[i,j]==1) {
-    fdr.downstream.matrix[i,j] <- HRU.matrix[i,j+1]
-  } else if (fdr.matrix[i,j]==2) {
-    fdr.downstream.matrix[i,j] <- HRU.matrix[i-1,j+1]
-  } else if (fdr.matrix[i,j]==4) {
-    fdr.downstream.matrix[i,j] <- HRU.matrix[i-1,j]
-  } else if (fdr.matrix[i,j]==8) {
-    fdr.downstream.matrix[i,j] <- HRU.matrix[i-1,j-1]
-  } else if (fdr.matrix[i,j]==16) {
-    fdr.downstream.matrix[i,j] <- HRU.matrix[i,j-1]
-  } else if (fdr.matrix[i,j]==32) {
-    fdr.downstream.matrix[i,j] <- HRU.matrix[i+1,j-1]
-  } else if (fdr.matrix[i,j]==64) {
-    fdr.downstream.matrix[i,j] <- HRU.matrix[i+1,j]
-  } else if (fdr.matrix[i,j]==128) {
-    fdr.downstream.matrix[i,j] <- HRU.matrix[i+1,j+1]
-  } else {
-    fdr.downstream.matrix[i,j] <- NA
-  }
-}
-  
+rownames(weighting.matrix) <- unique(FR.HRU)
+colnames(weighting.matrix) <- unique(FR.HRU)
 
-
-if (fdr.matrix[i,j]==1) {
-  fdr.downstream.matrix[i,j] <- HRU.matrix[i,j+1]
-} else if (fdr.matrix[i,j]==2) {
-  fdr.downstream.matrix[i,j] <- HRU.matrix[i-1,j+1]
-} else if (fdr.matrix[i,j]==4) {
-  fdr.downstream.matrix[i,j] <- HRU.matrix[i-1,j]
-} else if (fdr.matrix[i,j]==8) {
-  fdr.downstream.matrix[i,j] <- HRU.matrix[i-1,j-1]
-} else if (fdr.matrix[i,j]==16) {
-  fdr.downstream.matrix[i,j] <- HRU.matrix[i,j-1]
-} else if (fdr.matrix[i,j]==32) {
-  fdr.downstream.matrix[i,j] <- HRU.matrix[i+1,j-1]
-} else if (fdr.matrix[i,j]==64) {
-  fdr.downstream.matrix[i,j] <- HRU.matrix[i+1,j]
-} else if (fdr.matrix[i,j]==128) {
-  fdr.downstream.matrix[i,j] <- HRU.matrix[i+1,j+1]
-} else {
-  fdr.downstream.matrix[i,j] <- NA
-}
+rowSums(brompton$disc$weights)
