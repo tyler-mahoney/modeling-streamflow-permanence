8401c036749b50ffdcb6fc487aed6ee8afb69096
tyler-mahoney <tyler.mahoney@louisville.edu>
2022-07-19 09:04:12 -0400

    Uploaded geodatabase, created script to formulate HRUs

--- downstream-cells-quantification.R
+++ downstream-cells-quantification.R
@@ -1,14 +1,50 @@
 library(sp)
 library(raster)
+library(dynatopmodel)
+library(topmodel)
+
 
 FR.HRU <- raster('SpatialInputData/FallingRockHRUs.tif')
 FR.fdr <- raster('SpatialInputData/fr_fdr')
+FR.dem <- raster('SpatialInputData/fr1meterDEM.tif')
+FR.soils <- raster('SpatialInputData/FRSoils1mMask.tif')
+FR.fac.dinf <- raster('SpatialInputData/fr_fac_dinf.tif')
+TWI.dinf <- raster('C:/Users/david/OneDrive/Documents/ArcGIS/Projects/FallingRock/twi_dinf.tif')
 
 HRU.matrix <- as.matrix(FR.HRU)
 fdr.matrix <- as.matrix(FR.fdr)
+dem.matrix <- as.matrix(FR.dem)
+soils.matrix <- as.matrix(FR.soils)
+TWI.dinf.matrix <- as.matrix(TWI.dinf)
+TWI.dinf.matrix[TWI.dinf.matrix==0] <- NA
+
+twi <- build_layers(FR.dem)
+twi <- twi$atb
+
+normalize.matrix <- function(in.matrix) {
+  out.matrix <- (in.matrix-min(in.matrix,na.rm=T))/(max(in.matrix,na.rm=T)-min(in.matrix,na.rm=T))
+  return(out.matrix)
+}
+
+twi.dist <- normalize.matrix(as.matrix(twi$atb))
+twi.hist <- hist(twi.dist,plot = F)
+
+twi.dist.2 <- as.matrix(FR.TWI.calc)
+twi.dist.2[twi.dist.2 == 0] <- NA
+twi.dist.2 <- normalize.matrix(twi.dist.2)
+twi.hist.2 <- hist(twi.dist.2, plot = F)
+
+c1 <- rgb(173,216,230,max = 255, alpha = 80, names = "lt.blue")
+c2 <- rgb(255,192,203, max = 255, alpha = 80, names = "lt.pink")
+
+
+plot(twi.hist,col=c3)
+plot(twi.hist.dinf,col=c1,add=T)
+
+
 
 fdr.downstream.matrix <- matrix(nrow=nrow(HRU.matrix),ncol=ncol(HRU.matrix))
-
+ 
 
 for (i in 1:nrow(HRU.matrix)) {
   for (j in 1:ncol(HRU.matrix)) {
@@ -38,14 +74,15 @@ for (i in 1:nrow(HRU.matrix)) {
   }
 }
 
-downstream.weighting.matrix <- matrix(nrow=length(unique(FR.HRU)),ncol=length(unique(FR.HRU)))
-
+try.downstream.weighting.matrix <- matrix(nrow=length(unique(FR.HRU)),ncol=length(unique(FR.HRU)))
+(start <- Sys.time())
 for (i in 1:length(unique(FR.HRU))) {
+  print(unique(FR.HRU)[i])
   for (j in 1:length(unique(FR.HRU))) {
-    print(unique(FR.HRU)[i])
-    downstream.weighting.matrix[i,j] <- length(which((HRU.matrix==unique(FR.HRU)[i])&(fdr.downstream.matrix==unique(FR.HRU)[j])))
+    try.downstream.weighting.matrix[i,j] <- length(which((HRU.matrix==unique(FR.HRU)[i])&(fdr.downstream.matrix==unique(FR.HRU)[j])))
   }
 }
+(end <- Sys.time())
 
 options("scipen"=100, "digits"=5)
 
@@ -54,7 +91,9 @@ colnames(downstream.weighting.matrix) <- unique(FR.HRU)
 
 weighting.matrix <- downstream.weighting.matrix/rowSums(downstream.weighting.matrix)
 
-rownames(weighting.matrix) <- unique(FR.HRU)
-colnames(weighting.matrix) <- unique(FR.HRU)
+## Run Kmeans
+set.seed(123)
+plot(x=TWI.dinf.matrix,y=soils.matrix)
 
-rowSums(brompton$disc$weights)
+Fr.kmeans.df <- data.frame('TWI'=as.vector(TWI.dinf.matrix),'soils'= as.vector(soils.matrix))
+Fr.kmeans <- kmeans(Fr.kmeans.df,centers = 20, iter.max = 5, nstart = 10)
